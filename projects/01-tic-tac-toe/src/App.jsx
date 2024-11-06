97e792b3f5a8fc0ea48917935e99b79d8df9e23e
Neutras <byron.estayc@gmail.com>
2024-11-06 19:58:31 -0300

    Extract to Components

---
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import confetti from "canvas-confetti";
+
+import { Square } from "./components/Square.jsx";
+import { TURNS } from "./constants.js";
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { WinnerModal } from "./components/WinnerModal.jsx";
+
+
+function App() {
+  // Estado Del Tablero
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  
+  // Null = No hay ganador || False = Empate
+  const [winner, setWinner] = useState(null);
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+
+  const updateBoard = (index) => {
+    // No actualizar posición si hay contenido o si hay ganador
+    if (board[index] || winner) return;
+    // Actualizar Tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    // Cambiar Turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    // Revisar si hay ganador
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
+    }
+  };
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset</button>
+      <section className="game">
+        {board.map((square, index) => (
+          <Square
+            key={index}
+            index={index}
+            updateBoard={updateBoard}
+          >
+            {square}
+          </Square>
+        ))}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner}/>
+    </main>
+  );
+}
+
+export default App;
---
+++ projects/01-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,23 @@
+import { WINNER_COMBOS } from "../constants";
+
+export const checkWinnerFrom = (boardToCheck) => {
+    // Revisa las posiciones ganadoras
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]; // X u O
+      }
+    }
+    // Si no hay ganador
+    return null;
+  };
+
+export const checkEndGame = (newBoard) => {
+    // Revisa si hay un empate
+    // Si no hay mas espacios vacios y en el tablero
+    return newBoard.every((square) => square !== null)
+  }
